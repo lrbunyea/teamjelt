3d0165621a657b1f055fb80ad85facf1d5aca5c6
Tariq <trakha10899@gmail.com>
2019-08-28 18:55:37 -0400

    changes

---
+++ RedLightGreenLight/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            TimerEnded();
+        }
+    }
+
+    void TimerEnded()
+    {
+        //load Game Over screen
+    }
+}
--- RedLightGreenLight/Assets/Scripts/WorldManager.cs
+++ RedLightGreenLight/Assets/Scripts/WorldManager.cs
@@ -1,22 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldManager : MonoBehaviour
 {
-    #region Variables
-    public StateManager.State currentState;
-    #endregion
+    public Boolean isRed;
+    Boolean isControllerMoving = false;
 
-    #region Unity API
+    // Start is called before the first frame update
     void Start()
     {
-        
+        //set up dragon image
+        //load inital loading screen
+        //take player inputs
+
+        /*if(player is loaded in)
+         * {
+         *      change player image
+         * }       
+
+        if(dragon is loaded in)
+         * {
+         *      load game screen
+         *      activate timer       
+         * }*/
     }
 
+    // Update is called once per frame
     void Update()
     {
-        //Button presses here...
+        if (isRed)
+        {
+            //change dragon image
+            //stop time counter
+
+            if (isControllerMoving)
+            {
+                //change player image
+                //send vibration
+            }
+        }
+        else
+        {
+            //change dragon image
+            //allow movement
+            //call time script
+        }
     }
-    #endregion
 }
